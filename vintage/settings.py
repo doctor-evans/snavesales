444529a02af7a3123f64817a8ab172860c0abac6
Evans <evanschan200@gmail.com>
2024-11-17 19:13:56 +0100

    price product

--- vintage/settings.py
+++ vintage/settings.py
@@ -66,20 +66,20 @@ WSGI_APPLICATION = "vintage.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
-# Replace the SQLite DATABASES configuration with PostgreSQL:
 # DATABASES = {
-#     'default': dj_database_url.config(
-#         # Replace this value with your local database's connection string.
-#         default='postgresql://postgres:postgres@localhost:5432/vintage',
-#         conn_max_age=600
-#     )
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
 # }
+# Replace the SQLite DATABASES configuration with PostgreSQL:
+DATABASES = {
+    'default': dj_database_url.config(
+        # Replace this value with your local database's connection string.
+        default='postgresql://postgres:postgres@localhost:5432/vintage',
+        conn_max_age=600
+    )
+}
 
 
 # Password validation
